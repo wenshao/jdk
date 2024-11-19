8df84edb1d310feff8ef120e6e873c7429623d9f
Shaojin Wen <shaojin.wensj@alibaba-inc.com>
2024-11-19 09:31:57 +0800

    add benchmark

--- test/micro/org/openjdk/bench/java/lang/Longs.java
+++ test/micro/org/openjdk/bench/java/lang/Longs.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -75,6 +75,22 @@ public class Longs {
     public void toStringSmall(Blackhole bh) {
         for (long value : longArraySmall) {
             bh.consume(Long.toString(value));
+        }
+    }
+
+    /** Performs toHexString on small values, just a couple of digits. */
+    @Benchmark
+    public void toHexStringSmall(Blackhole bh) {
+        for (long value : longArraySmall) {
+            bh.consume(Long.toHexString(value));
+        }
+    }
+
+    /** Performs toHexString on large values */
+    @Benchmark
+    public void toHexStringBig(Blackhole bh) {
+        for (long value : longArrayBig) {
+            bh.consume(Long.toHexString(value));
         }
     }
 
